3ef016250faf02d53ca0a1898100fe7159fbc452
HOYEONCHOI <chy1995@ajou.ac.kr>
2021-11-30 23:34:45 +0900

    의존관계 자동 주입

--- main/java/hello/core/AutoAppConfig.java
+++ main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
+@Configuration
 @ComponentScan(
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
