df5ae316358d66665760b9f754c18eda804af8b1
HOYEONCHOI <chy1995@ajou.ac.kr>
2021-12-02 23:28:42 +0900

    빈 생명주기 콜백

---
+++ test/java/hello/core/lifecycle/NetworkClient.java
@@ -0,0 +1,74 @@
+package hello.core.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient /*implements InitializingBean, DisposableBean*/ {
+
+    private String url;
+
+    public NetworkClient(){
+        System.out.println("생성자 호출, url = " + url);
+
+    }
+
+    public void setUrl(String url){
+        this.url = url;
+    }
+
+    // 서비스 시작시 호출
+    public void connect(){
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message){
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    // 서비스 종료시 호출
+    public void disconnect(){
+        System.out.println("close: " + url);
+    }
+
+/*  1번방식  *//*implements InitializingBean, DisposableBean*//*
+    // 의존관계 주입이 끝나면 호출하는 부분
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }*/
+
+    /* 2번 방식 - 의존관계 주입이 끝나면 호출하는 부분
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }*/
+
+    // 3번 방식 - PostConstruct, PreDestroy <- 그냥 3번 방식 쓰면 됨.
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+}
